58e0b54a86efb60ee2ae5532a163c520b5baac66
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-02-10 14:41:10 -0500

    Intial app

---
+++ HyTracker/HyTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HyTracker
+//
+//  Created by Jeffrey Sweeney on 2/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HyTracker/HyTracker/HyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyTrackerApp.swift
+//  HyTracker
+//
+//  Created by Jeffrey Sweeney on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
