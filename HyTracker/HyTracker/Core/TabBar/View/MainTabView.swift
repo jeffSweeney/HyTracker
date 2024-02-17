7f7dbcb60e000dfeb26235b27b7645ab1a638b15
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-02-17 18:20:21 -0500

    Add template for main tab view

---
+++ HyTracker/HyTracker/Core/TabBar/View/MainTabView.swift
@@ -0,0 +1,22 @@
+//
+//  MainTabView.swift
+//  HyTracker
+//
+//  Created by Jeffrey Sweeney on 2/17/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        VStack {
+            HTLogoView(size: .normal)
+            
+            Text("UNDER CONSTRUCTION: Main Tab View")
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
