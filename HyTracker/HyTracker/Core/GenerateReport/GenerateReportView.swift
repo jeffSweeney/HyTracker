21a124cf7ec6e85c5c0c2bb844bf71a3da1a75ba
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-02-25 20:17:52 -0500

    Build GenerateReportView

--- HyTracker/HyTracker/Core/GenerateReport/GenerateReportView.swift
+++ HyTracker/HyTracker/Core/GenerateReport/GenerateReportView.swift
@@ -15,13 +15,48 @@ struct GenerateReportView: View {
             HTLogoView(size: .normal)
                 .padding(.vertical)
             
-            VStack(spacing: 8) {
-                Text("GenerateReportView")
-                Text("UNDER CONSTRUCTION")
+            VStack(spacing: 16) {
+                Text("Generating a report will examine all non-exempt eligible workdays in your provided range, and calculate the percentage of those days you were in-office.")
+                
+                // TODO: The code below might be better in the report sheet?
+//                HStack(spacing: 2) {
+//                    Text("Your target percentage is determined by your weekly commitment (") +
+//                    Text("\(viewModel.weeklyRequirementTotal) days").bold() +
+//                    Text(") divided by the number of eligible days (") +
+//                    Text(viewModel.eligibleDaysSorted).bold() +
+//                    Text(") in a typical work week of non-exempt days.")
+//                }
+//                
+//                HStack(spacing: 2) {
+//                    Text("Target percentage: ") +
+//                    Text(viewModel.targetPercentage).bold()
+//                }
             }
-            .padding(.vertical)
+            .font(.subheadline)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal, 24)
             
-            Text("Welcome, \(viewModel.user.fullname)!")
+            VStack(spacing: 24) {
+                DatePicker(selection: $viewModel.reportStartDate, in: viewModel.reportStartRange, displayedComponents: .date) {
+                    Text("Start Range:")
+                        .font(.headline)
+                }
+                
+                DatePicker(selection: $viewModel.reportEndDate, in: viewModel.reportEndRange, displayedComponents: .date) {
+                    Text("End Range:")
+                        .font(.headline)
+                }
+            }
+            .padding()
+            .padding(.horizontal, 24)
+            
+            
+            Button(action: {
+                print("DEBUG: Tapped Generate Report")
+            }, label: {
+                HTPrimaryButton(screen: .generateReport, isActionable: true)
+            })
+            .padding(.top, 40)
         }
         .fontDesign(.serif)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -30,5 +65,5 @@ struct GenerateReportView: View {
 }
 
 #Preview {
-    GenerateReportView(viewModel: MainTabViewModel(user: User.BASIC_MOCK_USER))
+    GenerateReportView(viewModel: MainTabViewModel(user: User.REPORT_MOCK_USER))
 }
