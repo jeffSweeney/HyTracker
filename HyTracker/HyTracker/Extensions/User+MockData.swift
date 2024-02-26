0bd84fa9f443e8aa7cf3ca6d414f03baa8e2df4a
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-02-25 20:15:42 -0500

    Update user test mocks

--- HyTracker/HyTracker/Extensions/User+MockData.swift
+++ HyTracker/HyTracker/Extensions/User+MockData.swift
@@ -8,14 +8,25 @@
 import Foundation
 
 extension User {
+    fileprivate static var today: Date { return Date.now }
+    fileprivate static var oneMonthAgo: Date { Calendar.current.date(byAdding: .month, value: -1, to: today)! }
+    
     static let BASIC_MOCK_USER = User(id: UUID().uuidString, email: "Jeff@test.com", fullname: "Jeff Sweeney", hasOnboarded: true)
     
     static let PROFILE_MOCK_USER = User(id: UUID().uuidString,
                                         email: "Jeff@test.com",
                                         fullname: "Jeff Sweeney",
                                         hasOnboarded: true,
-                                        startDate: Date(),
+                                        startDate: Self.today,
                                         eligibleDays: [.monday, .tuesday, .wednesday, .thursday, .friday],
                                         weeklyRequirementTotal: 2
     )
+    
+    static let REPORT_MOCK_USER = User(id: UUID().uuidString,
+                                       email: "Jeff@test.com",
+                                       fullname: "Jeff Sweeney",
+                                       hasOnboarded: true,
+                                       startDate: Self.oneMonthAgo,
+                                       eligibleDays: [.tuesday, .wednesday, .thursday],
+                                       weeklyRequirementTotal: 2)
 }
