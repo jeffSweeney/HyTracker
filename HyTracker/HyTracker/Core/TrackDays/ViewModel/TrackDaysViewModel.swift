8a7ae5e97cbf6a4f65c37d67e6f75d39f23feeb4
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-03-09 16:36:32 -0500

    Define initial structure of new TrackDaysViewModel

--- HyTracker/HyTracker/Core/TrackDays/ViewModel/TrackDaysViewModel.swift
+++ HyTracker/HyTracker/Core/TrackDays/ViewModel/TrackDaysViewModel.swift
@@ -5,4 +5,26 @@
 //  Created by Jeffrey Sweeney on 3/9/24.
 //
 
+import Combine
 import Foundation
+
+class TrackDaysViewModel: ObservableObject {
+    // MARK: - Publishers
+    @Published var user: User
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init(user: User) {
+        self.user = user
+        
+        setupSubscribers()
+    }
+    
+    // MARK: Subscribers
+    private func setupSubscribers() {
+        UserService.shared.$currentUser
+            .compactMap { $0 } // Do not allow nil sets
+            .assign(to: \.user, on: self)
+            .store(in: &cancellables)
+    }
+}
